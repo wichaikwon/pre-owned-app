2e345fb645b153d1a3178e84af29944536451f1c
wichaikwon <wichaikwon@gmail.com>
2025-03-26 17:53:03 +0700

    update api URL

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
-export const pathBrandsAPI = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8081'
-export const pathDefectsAPI = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8084'
-export const pathPhonesAPI = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8086'
-export const pathLoginAPI = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8088'
+export const pathBrandsAPI = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://pre-owned-brand-service-production.up.railway.app'
+export const pathDefectsAPI = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://pre-owned-defect-service-production.up.railway.app'
+export const pathPhonesAPI = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://pre-owned-phone-service-production.up.railway.app'
+export const pathLoginAPI = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://pre-owned-auth-service-production.up.railway.app'
 
